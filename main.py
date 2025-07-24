fdc37ea21ceb0a310cfebb4f8f94675cb62e2db1
Alejandro González <alexgonzorod@hotmail.com>
2025-07-24 17:18:20 +0100

    completed the counter and sorting functions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_num_words
 from stats import count_characters
+from stats import sort_on
 
 def get_book_text(path_to_file):
 
@@ -10,12 +11,22 @@ def get_book_text(path_to_file):
 
 def main(path_to_file):
     book = get_book_text(path_to_file)
-    char_count = count_characters(book)
-    
     word_count = get_num_words(book)
+    char_count = count_characters(book)
+    sorted_chars = sort_on(char_count)
 
-    print(word_count)
-    print(char_count)
+    print("============ BOOKBOT ============"
+          + f"\nAnalyzing book found at {path_to_file}..."
+          + "\n----------- Word Count ----------"
+          + f"\n{word_count}"
+          + "\n--------- Character Count -------")
+    
+    for i in range(0, len(sorted_chars)):
+        print(f"{sorted_chars[i]["char"]}: {sorted_chars[i]["num"]}")
+    
+    print("\n============= END ===============")
+
+    
 
 
 main("books/frankenstein.txt")
--- stats.py
+++ stats.py
@@ -1,6 +1,6 @@
 def get_num_words(contents):
     num_words = contents.split()
-    return f"{len(num_words)} words found in the document"
+    return f"Found {len(num_words)} total words"
 
 def count_characters(book):
     my_dict = {}
@@ -12,3 +12,20 @@ def count_characters(book):
             my_dict[char.lower()] = 1
     
     return my_dict
+
+def sort_on(items):
+    
+    dict_list = []
+    my_max = float('-inf')
+
+    for item in items:
+        if(item.isalpha()):
+            if(items[item] > my_max):
+                dict_list.insert(0, {"char": item, "num": items[item]})
+                my_max = items[item]
+            else:
+                dict_list.append({"char": item, "num": items[item]})
+
+
+    return dict_list
+
