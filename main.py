4520db32eda2c20f6d966a9390285c755fd90fdb
Alejandro González <alexgonzorod@hotmail.com>
2025-07-24 14:30:58 +0100

    working on basic functions to analyse the content of files

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    
+    return file_contents
+
+def count_words(text):
+    contents = get_book_text(text)
+    num_words = contents.split()
+
+    return f"{len(num_words)} words found in the document"
+
+
+def main(path_to_file):
+    contents = get_book_text(path_to_file)
+    print(contents)
+
+
+##main("books/frankenstein.txt")
+print(count_words("books/frankenstein.txt"))
